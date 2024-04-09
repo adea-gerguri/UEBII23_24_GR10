94a78629032af440ea14e13a87f21fe1118ca1d4
Euronii <99759785+Euronii@users.noreply.github.com>
2024-04-09 23:11:22 +0200

    .php qato
    .

--- MainFiles/AboutUs/Aboutus.php
+++ MainFiles/AboutUs/Aboutus.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php include '../HeaderFooter/header.html'?>
+    <?php include '../HeaderFooter/header.php`'?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
