37b9fe611c4040e1a7f753bbd8697c44ad9bdfc0
Gent Zhushi <gentzhushi01@gmail.com>
2024-04-09 09:37:01 +0200

    HAPI I PAR DINAMIK U KRY WOOWOOWOO!!!!
    YESIIIIIIIR TOTAL SALES GOING BRAZYY

--- AdminSide/dbConn/init.php
+++ AdminSide/dbConn/init.php
@@ -1,9 +1,15 @@
 <?php
 
 # qitu ja jep credentials per login qe me bo te krejt!
-$dsn = "mysql:host=localhost;dbname=web2";
+#$dsn = "mysql:host=localhost;port=3307;dbname=web2";
+#$username = "root";
+#$password = "gentzhushi";
+
+$servername= "localhost";
 $username = "root";
-$password = "gentzhushi";
+$password = "";
+$database = "web2";
+$port = 3307;
 
 
 # echo"<script>alert('shkun credsat!') </script>";
--- AdminSide/index/index.php
+++ AdminSide/index/index.php
@@ -28,33 +28,39 @@
                 <div class="card">
                     <h3>Total Sales</h3>
                     <p>
-                        $ <!-- si prefix qe neve me na dal veq vlera e numrit mos mu pshtjell me bo me php -->
+                        <!-- si prefix qe neve me na dal veq vlera e numrit mos mu pshtjell me bo me php -->
                         <?php 
-                            include '../dbConn/init.php';
+                            include '../dbConn/init.php'; // na jep credentials
                             try {
-                                $conn = new PDO($dsn, $username, $password);
-                                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                                echo"<script> alert('U lidhem me databaze.')</script>";
+                                # echo"<script> console.log(\"test\") </script>";
+                                $conn = new mysqli($servername, $username, $password, $database, $port); # qitu osht errori
+                                echo"<script> console.log('U lidhem me databaze.')</script>";
                               
                                 # sql ngjashem si me java
-                                $sql = "select sum(price) from products;"; # qito me : perpara jon placeholders, sikur me jep vlera permes references n c++
+                                $sql = "select sum(price) as cmimiTotal from products;"; # qito me : perpara jon placeholders, sikur me jep vlera permes references n c++
                                 # ; # -> qikjo osht fiks per databazenn ton, veq e bojna uncomment
                                 $stmt = $conn->prepare($sql); # statement
                                 $stmt->execute();
-                                $rezultati = $stmt->fetch(PDO::FETCH_ASSOC);
-                                echo $rezultati;
-                              } catch(PDOException $e) {
-                                echo"<script>alert('Gabim gjate lidhjes me Databaze\n>>>".$e->getMessage()."') </script>";
+                                $rezultati = $stmt->get_result()->fetch_assoc();
+                                if($rezultati){
+                                    # var_dump($rezultati);
+                                    echo "$" . $rezultati['cmimiTotal'];
+                                } else {
+                                    echo 'Error 404 No Sales';
+                                }
+                              } catch(Exception $e) {
+                                echo "Error 404 No Database";
+                                echo"<script>console.log(\"Gabim gjate lidhjes me Databaze: " . $e->getMessage() . "\") </script>";
                               }
                         ?>
                     </p>
                 </div>
                 <div class="card">
-                    <h3>New Orders</h3>
+                    <h3>New Orders</h3> <!-- i njejti sen veq me query me interval t dites, maybe me intervval 1 javor -->
                     <p>15</p>
                 </div>
                 <div class="card">
-                    <h3>Low Stock Items</h3>
+                    <h3>Low Stock Items</h3> <!-- select count(id) from products where stock < 5 edhe shtine qaty posht-->
                     <p>5</p>
                 </div>
             </section>
@@ -62,9 +68,34 @@
             <section class="recent-activity">
                 <h2>Recent Activity</h2>
                 <ul>
-                    <li>Order #12345 placed by John Smith</li>
-                    <li>Product "T-Shirt" updated by Jane Doe</li>
-                    <li>New customer Sarah Jones registered</li>
+                    <li>Order #12345 
+                        <?php
+                            // qitu vjen ni query
+                            // select ID, fName, lName from ORDERS order by desc limit 1;
+                            // ruj krejt neper variabla,
+                            // echo $id;
+                        ?>
+                        placed by John Smith
+                        <?php
+                            // echo $fname . " " . $lName;
+                        ?>
+                    </li>
+
+                    <li>Product "T-Shirt" 
+                        <?php 
+                            // gjeje cili produkt u ndrru i fundit
+                            // select p.pName, admin.name from products p join audit a join users admin on p.id = a.id and admin.id = a.adminID order by desc limit 1;
+                            // ruje qat p.pName me ni variabel $productName edhe a.adminID ne $admini
+                            // echo $productName
+                        ?>
+                        updated by Jane Doe
+                        <?php
+                            // echo $admini;
+                        ?>
+                    
+                    </li>
+                    
+                    <li>New customer Sarah Jones registered</li> <!-- i njejti sen qitu shkon -->
                 </ul>
             </section>
         </main>
