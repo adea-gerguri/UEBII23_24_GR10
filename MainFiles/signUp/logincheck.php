66ae3d259622caf27b2bc3b6554c8270990fcc9f
Etnik-K <etnikkelmendi20@gmail.com>
2024-04-13 23:58:02 +0200

    Ndryshim
    Set cookies for email

--- MainFiles/signUp/logincheck.php
+++ MainFiles/signUp/logincheck.php
@@ -1,6 +1,7 @@
 <?php
+
 session_start();
-// Include the DatabaseUtil class for database connection
+
 require_once('./DatabaseUtil.php');
 
 // Check if the form is submitted
@@ -8,7 +9,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve form data
     $email = $_POST['email'];
     $password = $_POST['password'];
-
 
     try {
         // Get a database connection
@@ -19,16 +19,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt->execute([$email]);
         $user = $stmt->fetch();
        
-
         // Verify password
         if ($user && password_verify($password, $user['passwordi'])) {
             // Password is correct, set session variables and redirect to homepage
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['user_name'] = $user['firstName'];
            
+            // Set cookie for email
+            setcookie('email', $email, time() + (86400 * 30), "/"); // 30 days expiration
        
             header("Location: ../HomePage/index.php");
-           
             exit();
         } else {
             // Invalid email or password
@@ -39,4 +39,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Error: " . $e->getMessage();
     }
 }
+
+
+
+
+// session_start();
+// // Include the DatabaseUtil class for database connection
+// require_once('./DatabaseUtil.php');
+
+// // Check if the form is submitted
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     // Retrieve form data
+//     $email = $_POST['email'];
+//     $password = $_POST['password'];
+
+
+//     try {
+//         // Get a database connection
+//         $conn = DatabaseUtil::getConnection();
+
+//         // Prepare SQL statement to retrieve user data from the database
+//         $stmt = $conn->prepare("SELECT id, firstName, passwordi FROM Users WHERE email = ?");
+//         $stmt->execute([$email]);
+//         $user = $stmt->fetch();
+       
+
+//         // Verify password
+//         if ($user && password_verify($password, $user['passwordi'])) {
+//             // Password is correct, set session variables and redirect to homepage
+//             $_SESSION['user_id'] = $user['id'];
+//             $_SESSION['user_name'] = $user['firstName'];
+           
+       
+//             header("Location: ../HomePage/index.php");
+           
+//             exit();
+//         } else {
+//             // Invalid email or password
+//             echo "Invalid email or password";
+//         }
+//     } catch (PDOException $e) {
+//         // Handle database errors
+//         echo "Error: " . $e->getMessage();
+//     }
+// }
 ?>
