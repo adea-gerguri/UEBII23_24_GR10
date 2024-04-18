e3490eac5bf03546931d77a92b020a212de45b8a
Gresa-Hasani <153296296+Gresa-Hasani@users.noreply.github.com>
2024-04-19 00:34:56 +0200

    Update signUpcheck.php

--- MainFiles/signUp/signUpcheck.php
+++ MainFiles/signUp/signUpcheck.php
@@ -42,3 +42,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
+
+<?php
+// Marrja e vlerave nga forma
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$gender = $_POST['gender'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$confirmPassword = $_POST['confirmPassword'];
+$card_number = $_POST['card_number'];
+$expiration_date = $_POST['expiration_date'];
+$cvv = $_POST['cvv'];
+
+//manipulime me permbajtjen e stringjeve
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['firstName'])) {
+        $firstName = $_POST['firstName'];
+        $emri_pa_numra = preg_replace('/\d+/', '', $firstName);
+        echo  $emri_pa_numra . "<br>";
+    }
+}
+
+//validime për email
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["email"])) {
+        $email = $_POST["email"];
+
+        if (!preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', $email)) {
+            echo "<div class='error-message'>Please enter a valid email address.<br>";
+        } else {
+            echo "Email address '$email' is valid.<br>";
+        }
+    }
+}
+?>
