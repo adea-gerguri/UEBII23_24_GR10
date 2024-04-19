327f1aeacd94d412b7ed293d0688a68ab8747ec7
Gent Zhushi <gentzhushi01@gmail.com>
2024-04-19 02:49:55 +0200

    ADMIN/INDEX.PHP UPDATE🐶
    Indexi osht dinamik qitasht. Me kalimin e kohes na duhet me edit frontin edhe databazen po kshtu t dhanat qe jon qaty vijn prej databazes.!!!

--- AdminSide/index/index.php
+++ AdminSide/index/index.php
@@ -30,29 +30,31 @@
                     <p>
                         <!-- si prefix qe neve me na dal veq vlera e numrit mos mu pshtjell me bo me php -->
                         <?php 
+                            include "../dbConn/objects.php";
                             include '../dbConn/init.php'; // na jep credentials
                             try {
                                 # echo"<script> console.log(\"test\") </script>";
                                 # $conn = new mysqli($servername, $username, $password, $database, $port); # qitu osht errori
                                 $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
                                 $pdo = new PDO($dsn, $username, $password);
-                                echo"<script> console.log('U lidhem me databaze.')</script>";
+                                echo"<script> console.log('U lidhem me databaze per cmimiTotal.')</script>";
                                 
                                 # sql ngjashem si me java
                                 $sql = "select sum(price) as cmimiTotal from products;"; # qito me : perpara jon placeholders, sikur me jep vlera permes references n c++
                                 # ; # -> qikjo osht fiks per databazenn ton, veq e bojna uncomment
-                                echo"<script> console.log('ledri vula')</script>";
                                 $stmt = $pdo->query($sql); # statement
-                                #$stmt->execute();
-                                #$rezultati = $stmt->get_result()->fetch_assoc();
+                                $stmt->execute();   #$rezultati = $stmt->get_result()->fetch_assoc();
+
+                                $rezultati = $stmt->fetch(PDO::FETCH_ASSOC); # u perdor vargu i asocium!
+                                #echo"<script> console.log('ledri vula')</script>";
                                 if($rezultati){
-                                    # var_dump($rezultati);
-                                    echo "$" . $stmt['cmimiTotal'];
+                                    #var_dump($rezultati);
+                                    echo "$" . $rezultati["cmimiTotal"];
                                 } else {
-                                    echo 'Error 404 No Sales';
+                                    echo 'Error 404; No Result';
                                 }
                               } catch(Exception $e) {
-                                echo "Error 404 No Database";
+                                echo "Error 404; No Database Connection";
                                 echo"<script>console.log(\"Gabim ne [index.php] gjate lidhjes me Databaze: " . $e->getMessage() . "\") </script>";
                               }
                         ?>
@@ -60,17 +62,48 @@
                 </div>
                 <div class="card">
                     <h3>New Orders</h3> <!-- SELECT COUNT(invoiceID) FROM invoice WHERE invoiceDate >= NOW() - INTERVAL 1 MONTH; -->
-                    <p>/
+                    <p>
                         <?php
-                            // yuh yuh yuh;
+                            #include "../dbConn/objects.php";   // dmth spasmi nevoj dy her me bo include!!
+                            #include '../dbConn/init.php'; // na jep credentials
+                            try{
+                                $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+                                $pdo = new PDO($dsn, $username, $password);
+                                $sql = "SELECT COUNT(ID) AS numriPorosive FROM orders WHERE OrderDate >= CURDATE() - INTERVAL 1 WEEK;";
+                                $stmt = $pdo->query($sql);
+                                echo"<script>console.log(\"ledri vula\")</script>";
+                                $stmt->execute();
+                                $rezultati = $stmt->fetch(PDO::FETCH_ASSOC);
+                                if ($rezultati){
+                                    echo $rezultati["numriPorosive"];
+                                } else {
+                                    echo "No New Orders";
+                                }
+                            } catch (Exception $e){
+                                echo"Error 404; No Database Connection";
+                            }
                         ?>
                     </p>
                 </div>
                 <div class="card">
                     <h3>Low Stock Items</h3> <!-- SELECT COUNT(productID) FROM products WHERE stock < 5; edhe shtine qaty posht-->
-                    <p>/
+                    <p>
                         <?php
-                            //yuh yuh yuh;
+                            try{
+                                $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+                                $pdo = new PDO($dsn, $username, $password);
+                                $sql = "select count(productid) as produktetLowStock from stock where stock <= 5;";
+                                $stmt = $pdo->query($sql);
+                                $stmt->execute();
+                                $rezultati = $stmt->fetch(PDO::FETCH_ASSOC);
+                                if ($rezultati){
+                                    echo $rezultati["produktetLowStock"];
+                                } else {
+                                    echo "No New Orders";
+                                }
+                            } catch (Exception $e){
+                                echo"Error 404; No Database Connection";
+                            }
                         ?>
                     </p>
                 </div>
