3de669b6001a68ffcfe87d5b01a19a895a080087
Gent Zhushi <gentzhushi01@gmail.com>
2024-04-21 21:21:23 +0200

    update customers, products
    products u bi me funnksionu, customers title

--- AdminSide/customers/customers.php
+++ AdminSide/customers/customers.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../index/index-style.css">
-    <title>Document</title>
+    <title>RHAE Customers</title>
     <style>
         table{
             width: 80%;
