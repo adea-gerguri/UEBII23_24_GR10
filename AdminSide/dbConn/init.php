3e4f98379fb7b9bdd93d527f400f2da70070ff53
Gent Zhushi <gentzhushi01@gmail.com>
2024-05-28 23:50:12 +0200

    Update init.php
    formatim

--- AdminSide/dbConn/init.php
+++ AdminSide/dbConn/init.php
@@ -1,15 +1,6 @@
 <?php
-
-# qitu ja jep credentials per login qe me bo te krejt!
-#$dsn = "mysql:host=localhost;port=3307;dbname=web2";
-#$username = "root";
-#$password = "gentzhushi";
-
-$host= "localhost";
-$username = "root";
-$password = "";
-$dbname = "web2";
-$port = 3307;
-
-
-echo"<script>console.log('shkun credsat!') </script>";
+    $host= "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "web2";
+    $port = 3307;
