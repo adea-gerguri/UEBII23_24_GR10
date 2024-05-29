c0c59e65636e663bd773b79b61028485cda11861
Gent Zhushi <gentzhushi01@gmail.com>
2024-05-29 21:21:53 +0200

    Update queryFunction.php
    Co-Authored-By: Etnik-K <98020145+Etnik-K@users.noreply.github.com>
    Co-Authored-By: Euronii <99759785+Euronii@users.noreply.github.com>

--- AdminSide/index/queryFunction.php
+++ AdminSide/index/queryFunction.php
@@ -1,49 +1,45 @@
 <?php
 
-// include "./AdminSide/dbConn/init.php";
-// include "../dbConn/init.php";
+    function query(&$sql){
 
+        try {
 
-function query(&$sql){
+            // print_r($query);
 
-    try {
-
-        // print_r($query);
-
-        $con = mysqli_connect('localhost', 'root', '', 'web2', '3307');              
-        
-        // $sql = "SELECT order_id, customer_id, total, order_status, orderDate FROM Orders WHERE OrderDate >= CURDATE() - INTERVAL 1 WEEK ORDER BY orderDate DESC;";
-
-        // $resultSet = mysqli_query($con, $sql);
-        $resultSet = mysqli_query($con, $sql);
-
-
-
-        if(mysqli_num_rows($resultSet) > 0) {
-
-            while ($row = mysqli_fetch_assoc($resultSet)){
+            $con = mysqli_connect('localhost', 'root', '', 'web2', '3307');              
             
-                echo "<tr>";
-                echo "<td>" . $row["order_id"] . "</td>";
-                # echo "<td>" . $row["lastName"] . "</td>";
-                echo "<td>" . $row["customer_id"] . "</td>";
-                echo "<td>" . $row["total"] . "</td>";
-                echo "<td>" . $row["order_status"] . "</td>";
-                echo "<td>" . $row["orderDate"] . "</td>";
-                echo "</tr>";
-            
+            // $sql = "SELECT order_id, customer_id, total, order_status, orderDate FROM Orders WHERE OrderDate >= CURDATE() - INTERVAL 1 WEEK ORDER BY orderDate DESC;";
+
+            // $resultSet = mysqli_query($con, $sql);
+            $resultSet = mysqli_query($con, $sql);
+
+
+
+            if(mysqli_num_rows($resultSet) > 0) {
+
+                while ($row = mysqli_fetch_assoc($resultSet)){
+                
+                    echo "<tr>";
+                    echo "<td>" . $row["order_id"] . "</td>";
+                    # echo "<td>" . $row["lastName"] . "</td>";
+                    echo "<td>" . $row["customer_id"] . "</td>";
+                    echo "<td>" . $row["total"] . "</td>";
+                    echo "<td>" . $row["order_status"] . "</td>";
+                    echo "<td>" . $row["orderDate"] . "</td>";
+                    echo "</tr>";
+                
+                }
+
             }
+
+            $sql = "ZHUSHI ZHUSHIII";
+
+        } catch (Exception $e) {
+
+            echo "<tr colspan=\"4\"><td>" . $e->getMessage() . "</td></tr>";
 
         }
 
-        $sql = "ZHUSHI ZHUSHIII";
-
-    } catch (Exception $e) {
-
-        echo "<tr colspan=\"4\"><td>" . $e->getMessage() . "</td></tr>";
+        unset($query);
 
     }
-
-    unset($query);
-
-}
